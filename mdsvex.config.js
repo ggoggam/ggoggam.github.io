43fef239655ba16696cbf1e404ca6b388ebc3e1a
ggoggam <ohjoon0204@gmail.com>
2024-07-09 15:01:23 +0900

    Fixing katex for tex equations.

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -5,6 +5,39 @@ import remarkHeadings from '@vcarl/remark-headings';
 import preview, { textFormatter } from 'remark-preview';
 import readingTime from 'mdsvex-reading-time';
 import remarkFootnotes from 'remark-footnotes';
+import math from 'remark-math';
+import rehype_katex from 'rehype-katex';
+import katex from 'katex';
+import visit from 'unist-util-visit';
+
+const correct_hast_tree = () => (tree) => {
+	visit(tree, 'text', (node) => {
+		if (node.value.trim().startsWith('<')) {
+			node.type = 'raw';
+		}
+	});
+};
+
+const katex_blocks = () => (tree) => {
+	visit(tree, 'code', (node) => {
+		if (node.lang === 'math') {
+			const str = katex.renderToString(node.value, {
+				displayMode: true,
+				leqno: false,
+				fleqn: false,
+				throwOnError: true,
+				errorColor: '#cc0000',
+				strict: 'warn',
+				output: 'htmlAndMathml',
+				trust: false,
+				macros: { '\\f': '#1f(#2)' }
+			});
+
+			node.type = 'raw';
+			node.value = '{@html `' + str + '`}';
+		}
+	});
+}
 
 export default {
 	extensions: ['.md', '.svelte.md', '.svx'],
@@ -17,8 +50,12 @@ export default {
 		relativeImages,
 		remarkFootnotes,
 		readingTime,
+		math,
+		katex_blocks
 	],
 	rehypePlugins: [
+		correct_hast_tree,
+		rehype_katex,
 		slugPlugin,
 		[
 			autolinkHeadings,
