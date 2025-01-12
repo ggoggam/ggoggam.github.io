980e17da0c5e4bb2d71ae1462f5024aedd94fb10
Joon Kwon <ohjoon0204@gmail.com>
2025-01-12 05:12:33 +0000

    Reorganizing folders.

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -1,11 +1,11 @@
 import path from 'path'
 import fs from 'fs'
 
-const imageDirectory = path.join(process.cwd(), 'public', 'content', 'ggoggam')
+const imageDirectory = path.join(process.cwd(), 'public', 'ggoggam')
 
 export function getImages() {
     // get images from the public folder
     const fileNames = fs.readdirSync(imageDirectory)
     // return the images
-    return fileNames.map((name) => path.join('/content/ggoggam', name))
+    return fileNames.map((name) => path.join('/ggoggam', name))
 }
